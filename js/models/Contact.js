5f7a1bd7e01ac19de956bff85c66d94a820ed4b9
eNJi25 <nfischer.contact@gmail.com>
2024-07-26 16:56:13 +0100

    Ajout du model Class

--- js/models/Contact.js
+++ js/models/Contact.js
@@ -1,4 +1,12 @@
-export default class Contact {
+/**
+ * Contact représente un contact disposant d'un :
+ * - nom
+ * - prénom
+ * - téléphone
+ */
+
+export class Contact {
+
     constructor (nom, prenom, telephone){
         this.nom = nom;
         this.prenom = prenom;
